b6944d649d8b087b75fd6553aef45d8788146a11
Cocojajaj <131284833+Cocojajaj@users.noreply.github.com>
2024-05-18 17:20:54 +0200

    Update main.c

--- CAN_BUS Loopback/Core/Src/main.c
+++ CAN_BUS Loopback/Core/Src/main.c
@@ -16,6 +16,7 @@
   ******************************************************************************
   */
 /* USER CODE END Header */
+
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
@@ -40,7 +41,7 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-CAN_HandleTypeDef hcan;
+CAN_HandleTypeDef hcan; // Handle for CAN interface
 
 /* USER CODE BEGIN PV */
 
@@ -56,8 +57,8 @@ static void MX_CAN_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-CAN_TxHeaderTypeDef tx_header;
-CAN_RxHeaderTypeDef rx_header;
+CAN_TxHeaderTypeDef tx_header; // CAN transmit header
+CAN_RxHeaderTypeDef rx_header; // CAN receive header
 
 uint32_t tx_mailbox;
 uint8_t tx_data[8];
@@ -65,8 +66,9 @@ uint8_t rx_data[8];
 
 uint8_t count = 0;
 
+// Callback for message pending in receive FIFO 0
 void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan){
-	HAL_CAN_GetRxMessage(&hcan, CAN_RX_FIFO0, &rx_header, rx_data);
+	HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &rx_header, rx_data);
 }
 /* USER CODE END 0 */
 
@@ -76,40 +78,28 @@ void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan){
   */
 int main(void)
 {
-
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
   /* MCU Configuration--------------------------------------------------------*/
 
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
 
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
   /* Configure the system clock */
   SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_CAN_Init();
+
   /* USER CODE BEGIN 2 */
-  	HAL_CAN_Start(&hcan);
+    HAL_CAN_Start(&hcan); // Start CAN communication
+    HAL_CAN_ActivateNotification(&hcan, CAN_IT_RX_FIFO0_MSG_PENDING); // Enable notifications
 
-    HAL_CAN_ActivateNotification(&hcan, CAN_IT_RX_FIFO0_MSG_PENDING);
-
-    tx_header.DLC = 1; //
-    tx_header.ExtId = 0; // Is extended ID?
-    tx_header.IDE = CAN_ID_STD; //
-    tx_header.RTR = CAN_RTR_DATA; // Remote or data?
-    tx_header.StdId = 0x069; // Identifier
+    // Setup CAN transmit header
+    tx_header.DLC = 1; //Data length
+    tx_header.ExtId = 0; //Extended ID
+    tx_header.IDE = CAN_ID_STD; 
+    tx_header.RTR = CAN_RTR_DATA; 
+    tx_header.StdId = 0x069; // ID used by microcontroller
     tx_header.TransmitGlobalTime = DISABLE;
   /* USER CODE END 2 */
 
@@ -117,16 +107,17 @@ int main(void)
   /* USER CODE BEGIN WHILE */
     uint8_t value = 0;
 
-  while (1)
-  {
-    /* USER CODE END WHILE */
+    while (1)
+    {
+        // Prepare and send CAN message
+	    tx_data[0] = value; // Assign value to data array
+	    HAL_CAN_AddTxMessage(&hcan, &tx_header, tx_data, &tx_mailbox); // Send CAN message
+	    value++; // Increment value for next transmission
+	    HAL_Delay(100); 
+    }
+  /* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
-	  tx_data[0] = value;
-	  HAL_CAN_AddTxMessage(&hcan, &tx_header, tx_data, &tx_mailbox);
-	  value++;
-	  HAL_Delay(100);
-  }
+  /* USER CODE BEGIN 3 */
   /* USER CODE END 3 */
 }
 
@@ -139,9 +130,7 @@ void SystemClock_Config(void)
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+  /** Configure the main internal regulator output voltage */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
@@ -153,8 +142,7 @@ void SystemClock_Config(void)
     Error_Handler();
   }
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
+  /** Configure the clock for CPU, AHB, and APB buses */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
@@ -175,17 +163,9 @@ void SystemClock_Config(void)
   */
 static void MX_CAN_Init(void)
 {
-
-  /* USER CODE BEGIN CAN_Init 0 */
-
-  /* USER CODE END CAN_Init 0 */
-
-  /* USER CODE BEGIN CAN_Init 1 */
-
-  /* USER CODE END CAN_Init 1 */
   hcan.Instance = CAN;
   hcan.Init.Prescaler = 12;
-  hcan.Init.Mode = CAN_MODE_LOOPBACK;
+  hcan.Init.Mode = CAN_MODE_LOOPBACK; // Configured in loopback mode for testing
   hcan.Init.SyncJumpWidth = CAN_SJW_1TQ;
   hcan.Init.TimeSeg1 = CAN_BS1_3TQ;
   hcan.Init.TimeSeg2 = CAN_BS2_1TQ;
@@ -199,10 +179,6 @@ static void MX_CAN_Init(void)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN CAN_Init 2 */
-
-  /* USER CODE END CAN_Init 2 */
-
 }
 
 /**
@@ -212,17 +188,11 @@ static void MX_CAN_Init(void)
   */
 static void MX_GPIO_Init(void)
 {
-/* USER CODE BEGIN MX_GPIO_Init_1 */
-/* USER CODE END MX_GPIO_Init_1 */
-
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOF_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
-
-/* USER CODE BEGIN MX_GPIO_Init_2 */
-/* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
